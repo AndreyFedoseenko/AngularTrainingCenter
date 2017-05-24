f5a1c3fdc223e073b3b796e9c1390684b13700be
Andrey Fedosennko <fed.andrey@nixsolutions.com>
2017-05-24 12:35:50 +0300

    Create project and apply migrations

---
+++ CenterApi/AngularTrainingCenterApi/Context/TrainingCenterContext.cs
@@ -0,0 +1,22 @@
+﻿using AngularTrainingCenterApi.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularTrainingCenterApi.Context
+{
+    public class TrainingCenterContext : IdentityDbContext<ApplicationUser>
+    {
+        public TrainingCenterContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TrainingCenterContext Create()
+        {
+            return new TrainingCenterContext();
+        }
+    }
+}
